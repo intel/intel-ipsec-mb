intel/intel-ipsec-mb

commit 3d62708a90cbb67e864a68fb9572d9ec7f06810d

Windows x64 compilation option added. 'win_x64.mak' NMake files added. The change also forced some code updates due to toolchain differences between Linux and Windows.
Change-Id: I2850fdd3df7457ce84b245c1319afb3bf39c6682

win_x64.mak
@@ -1,0 +1,220 @@
+#
+# Copyright (c) 2017, Intel Corporation
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of Intel Corporation nor the names of its contributors
+#       may be used to endorse or promote products derived from this software
+#       without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+LIBNAME = libIPSec_MB.lib
+OBJ_DIR = obj
+
+!ifdef DEBUG
+DCFLAGS = /Od /DDEBUG /Zi /Yd
+DAFLAGS =
+!else
+DCFLAGS = /O2 /Oi
+DAFLAGS = -g
+!endif
+
+CC = cl
+CFLAGS = $(EXTRA_CFLAGS) $(DCFLAGS)  /I. /Iinclude \
+	/nologo /Y- /W3 /WX- /Gm- /Gy /fp:precise /EHsc
+
+LIB_TOOL = lib
+LIBFLAGS = /nologo /machine:X64 /nodefaultlib
+
+AS = nasm
+AFLAGS = $(DAFLAGS) -fwin64 -Xvc -DWIN_ABI -Iinclude/ -I./ -Iavx/ -Iavx2/ -Iavx512/ -Isse/
+
+lib_objs = \
+	$(OBJ_DIR)\aes128_cbc_dec_by4_sse.obj \
+	$(OBJ_DIR)\aes128_cbc_dec_by8_avx.obj \
+	$(OBJ_DIR)\aes128_cntr_by4_sse.obj \
+	$(OBJ_DIR)\aes128_cntr_by8_avx.obj \
+	$(OBJ_DIR)\aes128_ecbenc_x3.obj \
+	$(OBJ_DIR)\aes192_cbc_dec_by4_sse.obj \
+	$(OBJ_DIR)\aes192_cbc_dec_by8_avx.obj \
+	$(OBJ_DIR)\aes192_cntr_by4_sse.obj \
+	$(OBJ_DIR)\aes192_cntr_by8_avx.obj \
+	$(OBJ_DIR)\aes256_cbc_dec_by4_sse.obj \
+	$(OBJ_DIR)\aes256_cbc_dec_by8_avx.obj \
+	$(OBJ_DIR)\aes256_cntr_by4_sse.obj \
+	$(OBJ_DIR)\aes256_cntr_by8_avx.obj \
+	$(OBJ_DIR)\aes_cfb_128_sse.obj \
+	$(OBJ_DIR)\aes_cfb_128_avx.obj \
+	$(OBJ_DIR)\aes_cbc_enc_128_x4.obj \
+	$(OBJ_DIR)\aes_cbc_enc_128_x8.obj \
+	$(OBJ_DIR)\aes_cbc_enc_192_x4.obj \
+	$(OBJ_DIR)\aes_cbc_enc_192_x8.obj \
+	$(OBJ_DIR)\aes_cbc_enc_256_x4.obj \
+	$(OBJ_DIR)\aes_cbc_enc_256_x8.obj \
+	$(OBJ_DIR)\aes_keyexp_128.obj \
+	$(OBJ_DIR)\aes_keyexp_192.obj \
+	$(OBJ_DIR)\aes_keyexp_256.obj \
+	$(OBJ_DIR)\aes_xcbc_mac_128_x4.obj \
+	$(OBJ_DIR)\aes_xcbc_mac_128_x8.obj \
+	$(OBJ_DIR)\mb_mgr_aes192_flush_avx.obj \
+	$(OBJ_DIR)\mb_mgr_aes192_flush_sse.obj \
+	$(OBJ_DIR)\mb_mgr_aes192_submit_avx.obj \
+	$(OBJ_DIR)\mb_mgr_aes192_submit_sse.obj \
+	$(OBJ_DIR)\mb_mgr_aes256_flush_avx.obj \
+	$(OBJ_DIR)\mb_mgr_aes256_flush_sse.obj \
+	$(OBJ_DIR)\mb_mgr_aes256_submit_avx.obj \
+	$(OBJ_DIR)\mb_mgr_aes256_submit_sse.obj \
+	$(OBJ_DIR)\mb_mgr_aes_flush_avx.obj \
+	$(OBJ_DIR)\mb_mgr_aes_flush_sse.obj \
+	$(OBJ_DIR)\mb_mgr_aes_submit_avx.obj \
+	$(OBJ_DIR)\mb_mgr_aes_submit_sse.obj \
+	$(OBJ_DIR)\mb_mgr_aes_xcbc_flush_avx.obj \
+	$(OBJ_DIR)\mb_mgr_aes_xcbc_flush_sse.obj \
+	$(OBJ_DIR)\mb_mgr_aes_xcbc_submit_avx.obj \
+	$(OBJ_DIR)\mb_mgr_aes_xcbc_submit_sse.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_flush_avx.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_flush_avx2.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_flush_sse.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_flush_ni_sse.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_flush_avx512.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_md5_flush_avx.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_md5_flush_avx2.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_md5_flush_sse.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_md5_submit_avx.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_md5_submit_avx2.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_md5_submit_sse.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_sha_224_flush_avx.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_sha_224_flush_avx2.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_sha_224_flush_sse.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_sha_224_flush_ni_sse.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_sha_224_submit_avx.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_sha_224_submit_avx2.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_sha_224_submit_sse.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_sha_224_submit_ni_sse.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_sha_256_flush_avx.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_sha_256_flush_avx2.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_sha_256_flush_sse.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_sha_256_flush_ni_sse.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_sha_256_submit_avx.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_sha_256_submit_avx2.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_sha_256_submit_sse.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_sha_256_submit_ni_sse.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_sha_384_flush_avx.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_sha_384_flush_avx2.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_sha_384_flush_sse.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_sha_384_submit_avx.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_sha_384_submit_avx2.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_sha_384_submit_sse.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_sha_512_flush_avx.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_sha_512_flush_avx2.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_sha_512_flush_sse.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_sha_512_submit_avx.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_sha_512_submit_avx2.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_sha_512_submit_sse.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_submit_avx.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_submit_avx2.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_submit_sse.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_submit_ni_sse.obj \
+	$(OBJ_DIR)\mb_mgr_hmac_submit_avx512.obj \
+	$(OBJ_DIR)\md5_x4x2_avx.obj \
+	$(OBJ_DIR)\md5_x4x2_sse.obj \
+	$(OBJ_DIR)\md5_x8x2_avx2.obj \
+	$(OBJ_DIR)\save_xmms.obj \
+	$(OBJ_DIR)\sha1_mult_avx.obj \
+	$(OBJ_DIR)\sha1_mult_sse.obj \
+	$(OBJ_DIR)\sha1_ni_x2_sse.obj \
+	$(OBJ_DIR)\sha1_one_block_avx.obj \
+	$(OBJ_DIR)\sha1_one_block_sse.obj \
+	$(OBJ_DIR)\sha1_x8_avx2.obj \
+	$(OBJ_DIR)\sha1_x16_avx512.obj \
+	$(OBJ_DIR)\sha224_one_block_avx.obj \
+	$(OBJ_DIR)\sha224_one_block_sse.obj \
+	$(OBJ_DIR)\sha256_oct_avx2.obj \
+	$(OBJ_DIR)\sha256_one_block_avx.obj \
+	$(OBJ_DIR)\sha256_one_block_sse.obj \
+	$(OBJ_DIR)\sha256_ni_x2_sse.obj \
+	$(OBJ_DIR)\sha384_one_block_avx.obj \
+	$(OBJ_DIR)\sha384_one_block_sse.obj \
+	$(OBJ_DIR)\sha512_one_block_avx.obj \
+	$(OBJ_DIR)\sha512_one_block_sse.obj \
+	$(OBJ_DIR)\sha512_x2_avx.obj \
+	$(OBJ_DIR)\sha512_x2_sse.obj \
+	$(OBJ_DIR)\sha512_x4_avx2.obj \
+	$(OBJ_DIR)\sha_256_mult_avx.obj \
+	$(OBJ_DIR)\sha_256_mult_sse.obj \
+	$(OBJ_DIR)\aes_xcbc_expand_key.obj \
+	$(OBJ_DIR)\mb_mgr_avx.obj \
+	$(OBJ_DIR)\mb_mgr_avx2.obj \
+	$(OBJ_DIR)\mb_mgr_avx512.obj \
+	$(OBJ_DIR)\mb_mgr_sse.obj \
+	$(OBJ_DIR)\md5_one_block.obj
+
+gcm_objs = \
+        $(OBJ_DIR)\gcm_sse.obj \
+	$(OBJ_DIR)\gcm_avx_gen2.obj \
+	$(OBJ_DIR)\gcm_avx_gen4.obj
+
+all_objs = $(lib_objs) $(gcm_objs)
+
+all: $(LIBNAME)
+
+$(LIBNAME): $(all_objs)
+	$(LIB_TOOL) $(LIBFLAGS) /out:$@ $(all_objs)
+
+{.\}.c{$(OBJ_DIR)}.obj:
+	$(CC) /Fo$@ /c $(CFLAGS) $<
+
+{.\}.asm{$(OBJ_DIR)}.obj:
+	$(AS) -o $@ $(AFLAGS) $<
+
+{sse\}.c{$(OBJ_DIR)}.obj:
+	$(CC) /Fo$@ /c $(CFLAGS) $<
+
+{sse\}.asm{$(OBJ_DIR)}.obj:
+	$(AS) -o $@ $(AFLAGS) $<
+
+{avx\}.c{$(OBJ_DIR)}.obj:
+	$(CC) /Fo$@ /c $(CFLAGS) $<
+
+{avx\}.asm{$(OBJ_DIR)}.obj:
+	$(AS) -o $@ $(AFLAGS) $<
+
+{avx2\}.c{$(OBJ_DIR)}.obj:
+	$(CC) /Fo$@ /c $(CFLAGS) $<
+
+{avx2\}.asm{$(OBJ_DIR)}.obj:
+	$(AS) -o $@ $(AFLAGS) $<
+
+{avx512\}.c{$(OBJ_DIR)}.obj:
+	$(CC) /Fo$@ /c $(CFLAGS) $<
+
+{avx512\}.asm{$(OBJ_DIR)}.obj:
+	$(AS) -o $@ $(AFLAGS) $<
+
+{include\}.asm{$(OBJ_DIR)}.obj:
+	$(AS) -o $@ $(AFLAGS) $<
+
+$(OBJ_DIR):
+	mkdir $(OBJ_DIR)
+
+clean:
+	del /q $(all_objs)
+	del /q $(LIBNAME)
+

LibTestApp/win_x64.mak
@@ -1,0 +1,49 @@
+#
+# Copyright (c) 2017, Intel Corporation
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of Intel Corporation nor the names of its contributors
+#       may be used to endorse or promote products derived from this software
+#       without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+APP = ipsec_MB_testapp
+IPSECLIB = ..\libIPSec_MB.lib
+
+!ifdef DEBUG
+DCFLAGS = /Od /DDEBUG /Zi /Yd
+!else
+DCFLAGS = /O2 /Oi
+!endif
+
+CC = cl
+CFLAGS = /nologo $(DCFLAGS) /Y- /W3 /WX- /Gm- /Gy  /fp:precise /EHsc /I.. /I..\include
+
+all: $(APP)
+
+$(APP): main.obj $(IPSECLIB)
+	$(CC) /Fe$(APP) /MT main.obj $(IPSECLIB)
+
+main.obj: main.c do_test.h
+	$(CC) /c $(CFLAGS) main.c
+
+clean:
+	del /q main.obj $(APP)

LibTestApp/do_test.h
@@ -287,7 +287,6 @@ TEST_GCM(void)
         UINT8 auth_tag[12] = {0xf3,0x42,0x37,0xc7,0x14,0x62,0x4c,0xf6,
                               0xbe,0xa1,0x88,0xc0};
         gcm_data gdata;
-        DECLARE_ALIGNED(UINT64 exp_keys[2*11], 16);
         UINT8 text[27];
         UINT8 auth_test[12];
         int i;
@@ -296,6 +295,7 @@ TEST_GCM(void)
         printf("testing GCM\n");
 
         aes_keyexp_128_enc(keys128, gdata.expanded_keys);
+
         aesni_gcm_precomp(&gdata, hashSubKey);
 
         aesni_gcm_enc(&gdata, text, plaintext, sizeof(plaintext), iv,
@@ -344,7 +344,6 @@ TEST_AUX_FUNC(void)
 
         UINT32 digest1[8];
         UINT64 digest3[8];
-        UINT32 i;
         DECLARE_ALIGNED(UINT32 k1_exp[15*4],   16);
         DECLARE_ALIGNED(UINT32 k2[4],          16);
         DECLARE_ALIGNED(UINT32 k3[4],          16);

README
@@ -41,7 +41,8 @@ avx512 - Intel(R) AVX512 optimized routines
 Compilation
 ===========
 
-Linux
+Linux (64-bit only)
+-------------------
 
 Required tools:
 - GNU make
@@ -50,6 +51,21 @@ Required tools:
 
 Simply run "make" or "make all" at the top level directory to compile the library.
 To clean the library build please run "make clean".
+
+Windows (x64 only)
+------------------
+
+Required tools:
+- Microsoft (R) Visual Studio 2010:
+  - NMAKE: Microsoft (R) Program Maintenance Utility Version 10.00.30319.01
+  - CL: Microsoft (R) C/C++ Optimizing Compiler Version 16.00.30319.01 for x64
+  - LIB: Microsoft (R) Library Manager Version 10.00.30319.01
+  - LINK: Microsoft (R) Incremental Linker Version 10.00.30319.01
+- NASM version 2.12.02 (or newer)
+
+Simply run "nmake /f win_x64.mak" or "nmake /f win_x64.mak all" at the top level directory
+to compile the library. To clean the library build please run "nmake /f win_x64.mak clean".
+
 
 Legal Disclaimer
 ================

avx/aes_cfb_128_avx.asm
@@ -57,8 +57,8 @@
 %else
 %define arg1	rcx
 %define arg2	rdx
-%define arg3	rdi
-%define arg4	rsi
+%define arg3	r8
+%define arg4	r9
 %define arg5	[rsp + 5*8]
 %endif
 
@@ -73,7 +73,7 @@
 %define LEN	r11
 %endif
 
-%define TMP0	r9
+%define TMP0	rax
 %define TMP1	r10
 %define PTR0	rsp + _buffer
 

include/os.h
@@ -31,10 +31,13 @@
 #ifdef LINUX
 #define DECLARE_ALIGNED(decl, alignval) \
         decl __attribute__((aligned(alignval)))
-#define __forceinline static inline __attribute__((always_inline))
+#define __forceinline \
+        static inline __attribute__((always_inline))
 #else
 #define DECLARE_ALIGNED(decl, alignval) \
         __declspec(align(alignval)) decl
+#define __forceinline \
+        static __forceinline
 #endif
 
 #endif // end ifndef __OS_H

sse/aes_cfb_128_sse.asm
@@ -57,8 +57,8 @@
 %else
 %define arg1	rcx
 %define arg2	rdx
-%define arg3	rdi
-%define arg4	rsi
+%define arg3	r8
+%define arg4	r9
 %define arg5	[rsp + 5*8]
 %endif
 
@@ -73,7 +73,7 @@
 %define LEN	r11
 %endif
 
-%define TMP0	r9
+%define TMP0	rax
 %define TMP1	r10
 %define PTR0	rsp + _buffer
 

commit 9012b6b1a6144e86e2adb5c604f78ba75f4547bf

Fix dependency with obj directory for Windows x64
On the first run of nmake from a fresh clone:
nasm: fatal: unable to open output file `obj\aes128_cbc_dec_by4_sse.obj'

The obj directory must be done at first.

Fixes: 3d62708a90cb ("Windows x64 compilation option added...")
Signed-off-by: Jean-Mickael Guerin <jean-mickael.guerin@6wind.com>

win_x64.mak
@@ -188,6 +188,8 @@ all: $(LIBNAME)
 $(LIBNAME): $(all_objs)
 	$(LIB_TOOL) $(LIBFLAGS) /out:$@ $(all_objs)
 
+$(all_objs): $(OBJ_DIR)
+
 {.\}.c{$(OBJ_DIR)}.obj:
 	$(CC) /Fo$@ /c $(CFLAGS) $<
 