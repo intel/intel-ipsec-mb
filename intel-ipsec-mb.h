2be40305291301997292092c128c030dd4c3fde8
Marcel Cornu <marcel.d.cornu@intel.com>
2019-11-06 15:51:25 +0000

    lib: Updated library version string to 0.53.1-dev

--- intel-ipsec-mb.h
+++ intel-ipsec-mb.h
@@ -72,8 +72,8 @@ typedef struct {
 #endif
 
 /* Library version */
-#define IMB_VERSION_STR "0.53.0"
-#define IMB_VERSION_NUM 0x3500
+#define IMB_VERSION_STR "0.53.1-dev"
+#define IMB_VERSION_NUM 0x3501
 
 /* Macro to translate version number */
 #define IMB_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
