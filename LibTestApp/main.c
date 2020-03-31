e5e958461d8c4c8ae2313a5320756c6aca8dcc72
Marcel D Cornu <marcel.d.cornu@intel.com>
2020-04-01 00:36:05 +0100

    LibTestApp: Added imb_clear_mem() API test

--- LibTestApp/main.c
+++ LibTestApp/main.c
@@ -52,6 +52,7 @@ extern int zuc_test(const enum arch_type arch, struct IMB_MGR *mb_mgr);
 extern int kasumi_test(const enum arch_type arch, struct IMB_MGR *mb_mgr);
 extern int snow3g_test(const enum arch_type arch, struct IMB_MGR *mb_mgr);
 extern int direct_api_test(const enum arch_type arch, struct IMB_MGR *mb_mgr);
+extern int clear_mem_test(const enum arch_type arch, struct IMB_MGR *mb_mgr);
 
 #include "do_test.h"
 
@@ -301,6 +302,7 @@ main(int argc, char **argv)
                 errors += chained_test(atype, p_mgr);
                 errors += api_test(atype, p_mgr);
                 errors += direct_api_test(atype, p_mgr);
+                errors += clear_mem_test(atype, p_mgr);
                 free_mb_mgr(p_mgr);
         }
 
