cf716da2128b69067324f210c0dee27cf3f20693
Tomasz Kantecki <tomasz.kantecki@intel.com>
2020-04-20 14:01:57 +0100

    lib: version update to 0.54.1-dev

--- intel-ipsec-mb.h
+++ intel-ipsec-mb.h
@@ -72,8 +72,8 @@ typedef struct {
 #endif
 
 /* Library version */
-#define IMB_VERSION_STR "0.54.0"
-#define IMB_VERSION_NUM 0x3600
+#define IMB_VERSION_STR "0.54.1-dev"
+#define IMB_VERSION_NUM 0x3601
 
 /* Macro to translate version number */
 #define IMB_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
