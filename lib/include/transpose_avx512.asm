5a20f3fca18bd22bffb739f137ce34239d7f7d22
Tomasz Kantecki <tomasz.kantecki@intel.com>
2021-09-23 11:24:30 +0000

    avx512: add TRANSPOSE8_U32 macro variant for EVEX encoding

--- lib/include/transpose_avx512.asm
+++ lib/include/transpose_avx512.asm
@@ -473,6 +473,51 @@ PSHUFFLE_TRANSPOSE_MASK2: 	dq 0x0000000000000002
         vshufi64x2      %%IN15, %%H1, %%H3, 0xdd    ; R7
 %endmacro
 
+;; AVX512 variant of AVX2 TRANSPOSE8_U32 macro
+;; - vperm2i128/vperm2f128 replaced with vshufi32x4
+%macro TRANSPOSE8_U32_AVX512 10
+%define %%r0 %1   ;; [in/out] YMM with a0..a7
+%define %%r1 %2   ;; [in/out] YMM with b0..b7
+%define %%r2 %3   ;; [in/out] YMM with c0..c7
+%define %%r3 %4   ;; [in/out] YMM with d0..d7
+%define %%r4 %5   ;; [in/out] YMM with e0..e7
+%define %%r5 %6   ;; [in/out] YMM with f0..f7
+%define %%r6 %7   ;; [in/out] YMM with g0..g7
+%define %%r7 %8   ;; [in/out] YMM with h0..h7
+%define %%t0 %9   ;; [clobbered] temporary YMM
+%define %%t1 %10  ;; [clobbered] temporary YMM
+
+        ; process top half (r0..r3) {a...d}
+        vshufps %%t0, %%r0, %%r1, 0x44  ; t0 = {b5 b4 a5 a4   b1 b0 a1 a0}
+        vshufps %%r0, %%r0, %%r1, 0xEE  ; r0 = {b7 b6 a7 a6   b3 b2 a3 a2}
+        vshufps %%t1, %%r2, %%r3, 0x44  ; t1 = {d5 d4 c5 c4   d1 d0 c1 c0}
+        vshufps %%r2, %%r2, %%r3, 0xEE  ; r2 = {d7 d6 c7 c6   d3 d2 c3 c2}
+        vshufps %%r3, %%t0, %%t1, 0xDD  ; r3 = {d5 c5 b5 a5   d1 c1 b1 a1}
+        vshufps %%r1, %%r0, %%r2, 0x88  ; r1 = {d6 c6 b6 a6   d2 c2 b2 a2}
+        vshufps %%r0, %%r0, %%r2, 0xDD  ; r0 = {d7 c7 b7 a7   d3 c3 b3 a3}
+        vshufps %%t0, %%t0, %%t1, 0x88  ; t0 = {d4 c4 b4 a4   d0 c0 b0 a0}
+
+        ; use r2 in place of t0
+        ; process bottom half (r4..r7) {e...h}
+        vshufps %%r2, %%r4, %%r5, 0x44  ; r2 = {f5 f4 e5 e4   f1 f0 e1 e0}
+        vshufps %%r4, %%r4, %%r5, 0xEE  ; r4 = {f7 f6 e7 e6   f3 f2 e3 e2}
+        vshufps %%t1, %%r6, %%r7, 0x44  ; t1 = {h5 h4 g5 g4   h1 h0 g1 g0}
+        vshufps %%r6, %%r6, %%r7, 0xEE  ; r6 = {h7 h6 g7 g6   h3 h2 g3 g2}
+        vshufps %%r7, %%r2, %%t1, 0xDD  ; r7 = {h5 g5 f5 e5   h1 g1 f1 e1}
+        vshufps %%r5, %%r4, %%r6, 0x88  ; r5 = {h6 g6 f6 e6   h2 g2 f2 e2}
+        vshufps %%r4, %%r4, %%r6, 0xDD  ; r4 = {h7 g7 f7 e7   h3 g3 f3 e3}
+        vshufps %%t1, %%r2, %%t1, 0x88  ; t1 = {h4 g4 f4 e4   h0 g0 f0 e0}
+
+        vshufi32x4      %%r6, %%r1, %%r5, 0000_0011b ; h6...a6
+        vshufi32x4      %%r2, %%r1, %%r5, 0000_0000b ; h2...a2
+        vshufi32x4      %%r5, %%r3, %%r7, 0000_0011b ; h5...a5
+        vshufi32x4      %%r1, %%r3, %%r7, 0000_0000b ; h1...a1
+        vshufi32x4      %%r7, %%r0, %%r4, 0000_0011b ; h7...a7
+        vshufi32x4      %%r3, %%r0, %%r4, 0000_0000b ; h3...a3
+        vshufi32x4      %%r4, %%t0, %%t1, 0000_0011b ; h4...a4
+        vshufi32x4      %%r0, %%t0, %%t1, 0000_0000b ; h0...a0
+%endmacro
+
 ; LOAD ALL 8 LANES FOR 8x8 64-BIT TRANSPOSE
 ;
 ; r0-r7       [out] zmm registers which will contain the data to be transposed
