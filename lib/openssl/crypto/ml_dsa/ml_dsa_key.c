5a01297ba14e821d7585983d0d0fd2c303e3308c
vkarpenk <veronika.karpenko@intel.com>
2026-09-03 13:36:08 +0100

    lib: [ml-dsa] update copyright (#952)
    Signed-off-by: Veronika Karpenko <veronika.karpenko@intel.com>

--- lib/openssl/crypto/ml_dsa/ml_dsa_key.c
+++ lib/openssl/crypto/ml_dsa/ml_dsa_key.c
@@ -1,5 +1,6 @@
 /*
  * Copyright 2024-2025 The OpenSSL Project Authors. All Rights Reserved.
+ * Copyright (c) 2026 Intel Corporation. All Rights Reserved.
  *
  * Licensed under the Apache License 2.0 (the "License").  You may not use
  * this file except in compliance with the License.  You can obtain a copy
