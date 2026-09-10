03f9e2acb60ef987eb0e04a57fcee529d72d77fc
Tomasz Kantecki <tomasz.kantecki@intel.com>
2026-09-10 10:59:39 +0000

    avx512: [snow5g-nca] scrub key stream in safe data path
    Signed-off-by: Tomasz Kantecki <tomasz.kantecki@intel.com>

--- lib/avx512_t2/mb_mgr_snow5g_nca4_x2_submit_flush_vaes_avx512.asm
+++ lib/avx512_t2/mb_mgr_snow5g_nca4_x2_submit_flush_vaes_avx512.asm
@@ -328,6 +328,7 @@ align_label
         vpxorq  xmm0, xmm0, xmm0
         vmovdqa64 [rsp + _hqp_nca4], zmm0
         vmovdqa32 [rsp + _hqp_nca4 + 64], ymm0
+        vmovdqa32 [rsp + _keystream_nca4], ymm0
 %ifidn %%DIR, DECRYPT
         ;; _states_nca4 only written on decrypt path
         vmovdqu64 [rsp + _states_nca4 + 64 * 0], zmm0
