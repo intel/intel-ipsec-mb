432132109d2074a18726255665b74463e51da2ea
Marcel Cornu <marcel.d.cornu@intel.com>
2026-09-10 15:41:06 +0100

    lib: [HMAC-MD5] save/restore xmm6-xmm15 in SSE/AVX2 submit/flush
    Signed-off-by: Marcel Cornu <marcel.d.cornu@intel.com>

--- lib/avx2_t1/mb_mgr_hmac_md5_flush_avx2.asm
+++ lib/avx2_t1/mb_mgr_hmac_md5_flush_avx2.asm
@@ -99,6 +99,7 @@ mksection .text
 ; This routine and/or the called routine clobbers all GPRs
 struc STACK
 _gpr_save:      resq    8
+_xmm_save:      resq    20      ; xmm6-xmm15, Windows only
 _rsp_save:      resq    1
 endstruc
 
@@ -122,6 +123,16 @@ flush_job_hmac_md5_avx2:
 %ifndef LINUX
         mov     [rsp + _gpr_save + 8*6], rsi
         mov     [rsp + _gpr_save + 8*7], rdi
+        vmovdqa [rsp + _xmm_save + 16*0], xmm6
+        vmovdqa [rsp + _xmm_save + 16*1], xmm7
+        vmovdqa [rsp + _xmm_save + 16*2], xmm8
+        vmovdqa [rsp + _xmm_save + 16*3], xmm9
+        vmovdqa [rsp + _xmm_save + 16*4], xmm10
+        vmovdqa [rsp + _xmm_save + 16*5], xmm11
+        vmovdqa [rsp + _xmm_save + 16*6], xmm12
+        vmovdqa [rsp + _xmm_save + 16*7], xmm13
+        vmovdqa [rsp + _xmm_save + 16*8], xmm14
+        vmovdqa [rsp + _xmm_save + 16*9], xmm15
 %endif
         mov     [rsp + _rsp_save], rax  ; original SP
 
@@ -327,6 +338,16 @@ return:
 %ifndef LINUX
         mov     rsi, [rsp + _gpr_save + 8*6]
         mov     rdi, [rsp + _gpr_save + 8*7]
+        vmovdqa xmm6,  [rsp + _xmm_save + 16*0]
+        vmovdqa xmm7,  [rsp + _xmm_save + 16*1]
+        vmovdqa xmm8,  [rsp + _xmm_save + 16*2]
+        vmovdqa xmm9,  [rsp + _xmm_save + 16*3]
+        vmovdqa xmm10, [rsp + _xmm_save + 16*4]
+        vmovdqa xmm11, [rsp + _xmm_save + 16*5]
+        vmovdqa xmm12, [rsp + _xmm_save + 16*6]
+        vmovdqa xmm13, [rsp + _xmm_save + 16*7]
+        vmovdqa xmm14, [rsp + _xmm_save + 16*8]
+        vmovdqa xmm15, [rsp + _xmm_save + 16*9]
 %endif
         mov     rsp, [rsp + _rsp_save]  ; original SP
 
--- lib/avx2_t1/mb_mgr_hmac_md5_submit_avx2.asm
+++ lib/avx2_t1/mb_mgr_hmac_md5_submit_avx2.asm
@@ -66,6 +66,7 @@ extern md5_x8x2_avx2
 ; This routine and/or the called routine clobbers all GPRs
 struc STACK
 _gpr_save:      resq    8
+_xmm_save:      resq    20      ; xmm6-xmm15, Windows only
 _rsp_save:      resq    1
 endstruc
 
@@ -90,6 +91,16 @@ submit_job_hmac_md5_avx2:
 %ifndef LINUX
         mov     [rsp + _gpr_save + 8*6], rsi
         mov     [rsp + _gpr_save + 8*7], rdi
+        vmovdqa [rsp + _xmm_save + 16*0], xmm6
+        vmovdqa [rsp + _xmm_save + 16*1], xmm7
+        vmovdqa [rsp + _xmm_save + 16*2], xmm8
+        vmovdqa [rsp + _xmm_save + 16*3], xmm9
+        vmovdqa [rsp + _xmm_save + 16*4], xmm10
+        vmovdqa [rsp + _xmm_save + 16*5], xmm11
+        vmovdqa [rsp + _xmm_save + 16*6], xmm12
+        vmovdqa [rsp + _xmm_save + 16*7], xmm13
+        vmovdqa [rsp + _xmm_save + 16*8], xmm14
+        vmovdqa [rsp + _xmm_save + 16*9], xmm15
 %endif
         mov     [rsp + _rsp_save], rax  ; original SP
 
@@ -347,6 +358,16 @@ return:
 %ifndef LINUX
         mov     rsi, [rsp + _gpr_save + 8*6]
         mov     rdi, [rsp + _gpr_save + 8*7]
+        vmovdqa xmm6,  [rsp + _xmm_save + 16*0]
+        vmovdqa xmm7,  [rsp + _xmm_save + 16*1]
+        vmovdqa xmm8,  [rsp + _xmm_save + 16*2]
+        vmovdqa xmm9,  [rsp + _xmm_save + 16*3]
+        vmovdqa xmm10, [rsp + _xmm_save + 16*4]
+        vmovdqa xmm11, [rsp + _xmm_save + 16*5]
+        vmovdqa xmm12, [rsp + _xmm_save + 16*6]
+        vmovdqa xmm13, [rsp + _xmm_save + 16*7]
+        vmovdqa xmm14, [rsp + _xmm_save + 16*8]
+        vmovdqa xmm15, [rsp + _xmm_save + 16*9]
 %endif
         mov     rsp, [rsp + _rsp_save]  ; original SP
 
