5377d04e8389f1aed66e5699626bfc6c8a1e490b
Marcel Cornu <marcel.d.cornu@intel.com>
2026-09-15 17:21:42 +0100

    aes_t2: [AES-CFB] reserve xmm6-15 stack space on Windows only and skip redundant clears
    Signed-off-by: Marcel Cornu <marcel.d.cornu@intel.com>

--- lib/avx2_t2/mb_mgr_aes128_cfb_enc_avx2.asm
+++ lib/avx2_t2/mb_mgr_aes128_cfb_enc_avx2.asm
@@ -80,7 +80,9 @@ mksection .text
 ; This routine and its callee clobbers all GPRs
 struc STACK
 _gpr_save:      resq    8
+%ifndef LINUX
 _xmm_save:      resq    10*2
+%endif
 _rsp_save:      resq    1
 endstruc
 
@@ -436,7 +438,7 @@ align_label
 align_label
 %%done:
 %ifdef SAFE_DATA
-        clear_all_ymms_asm
+        clear_scratch_ymms_asm
 %else
         vzeroupper
 %endif ;; SAFE_DATA
--- lib/avx512_t2/mb_mgr_aes128_cfb_enc_flush_vaes_avx512.asm
+++ lib/avx512_t2/mb_mgr_aes128_cfb_enc_flush_vaes_avx512.asm
@@ -127,7 +127,9 @@ align_label
 ; This routine and its callee clobbers all GPRs
 struc STACK
 _gpr_save:      resq    8
+%ifndef LINUX
 _xmm_save:      resq    20      ; xmm6-xmm15, Windows only
+%endif
 _rsp_save:      resq    1
 endstruc
 
@@ -259,7 +261,7 @@ len_is_0:
 align_label
 return:
 %ifdef SAFE_DATA
-        clear_all_zmms_asm
+        clear_scratch_zmms_asm
 %else
         vzeroupper
 %endif ;; SAFE_DATA
