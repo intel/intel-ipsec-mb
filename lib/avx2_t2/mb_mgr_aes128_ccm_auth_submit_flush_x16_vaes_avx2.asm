13b51a3452deb9a1c95bd58ab1b288b39e794fb3
Marcel Cornu <marcel.d.cornu@intel.com>
2026-09-15 17:21:29 +0100

    aes_t1/t2: [AES-CCM] reserve xmm6-15 stack space on Windows only and skip redundant clears
    Signed-off-by: Marcel Cornu <marcel.d.cornu@intel.com>

--- lib/avx2_t2/mb_mgr_aes128_ccm_auth_submit_flush_x16_vaes_avx2.asm
+++ lib/avx2_t2/mb_mgr_aes128_ccm_auth_submit_flush_x16_vaes_avx2.asm
@@ -108,7 +108,9 @@ mksection .text
 ; This routine and its callee clobbers all GPRs
 struc STACK
 _gpr_save:      resq    8
+%ifndef LINUX
 _xmm_save:      resq    10*2
+%endif
 _rsp_save:      resq    1
 endstruc
 
@@ -690,7 +692,7 @@ align_label
 
 %%_done:
 %ifdef SAFE_DATA
-        clear_all_ymms_asm
+        clear_scratch_ymms_asm
 %else
         vzeroupper
 %endif ;; SAFE_DATA
