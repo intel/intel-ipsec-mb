4b9b9b9b4c8a40970d610158ba01977c4038eadb
Tomasz Kantecki <tomasz.kantecki@intel.com>
2026-09-16 11:18:21 +0000

    lib: [KASUMI-UEA1] use 64-bit cipher source offset in job API
    Offsets of 4GB and above were silently truncated.
    
    Signed-off-by: Tomasz Kantecki <tomasz.kantecki@intel.com>

--- lib/include/job_api_kasumi.h
+++ lib/include/job_api_kasumi.h
@@ -16,7 +16,7 @@ submit_kasumi_uea1_job(IMB_MGR *state, IMB_JOB *job)
         const kasumi_key_sched_t *key = job->enc_keys;
         const uint64_t iv = *(const uint64_t *) job->iv;
         const uint32_t msg_bytelen = (const uint32_t) job->msg_len_to_cipher_in_bytes;
-        const uint32_t msg_byteoff = (const uint32_t) job->cipher_start_src_offset_in_bytes;
+        const uint64_t msg_byteoff = job->cipher_start_src_offset_in_bytes;
         const void *src = job->src + msg_byteoff;
 
         CALL_KASUMI_F8_1_BUFFER(state, key, iv, src, job->dst, msg_bytelen);
